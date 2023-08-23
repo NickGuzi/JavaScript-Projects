75298dede227bbbc801753e37c147406b71f19c9
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-08-22 20:26:27 -0400

    Created files to practice with javascript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+document.write("Hello, World!"); //document.write practice 
+
+var A = "This is a string"; //Assigning variable practice
+document.write(A);
+
+var X = "HI!"; //Window alert practice
+window.alert(X);
+
+var Y = "I haven\'t had to use slashes before in code"; //String practice
+document.write(Y);
+
+document.write("\"If you know the way the wind blows,"
+    + " then you know the way you're headed.\""
+    + "- Me");
+                                                //Concatenation practice
+var B = "Concatenated" + "string";
+document.write(B);
+
+var Family = "Guzi-Dow's", Me = "Nick", Gf = "Sam", Brother = "Zach";
+document.write(Brother); //Multiple variable practice
+
+document.write(3 + 3);//Expression practice 
