32acb9e02678de5bfd9991f4e11c6aea10217f47
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-08-22 21:03:58 -0400

    Created Files to practice functions in JS

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+//Function Practice 
+function buttonFunction() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+// Concatenation Practice
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("combine").innerHTML = sentence;
+}
