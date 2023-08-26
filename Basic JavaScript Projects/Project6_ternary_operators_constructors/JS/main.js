cc465b2e378e1724e7ed4fede72894dc428336bf
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-08-25 23:01:02 -0400

    added more practice functions

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -1,3 +1,4 @@
+//Ternary operator practice
 function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
@@ -12,4 +13,33 @@ function VotingAge_Function() {
     Can_vote = (Vote < 18) ? "You are too young" : "You are old enough";
     document.getElementById("Voter").innerHTML = Can_vote + " to vote";
 
+}
+
+//constructor practice
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function keyCon() {
+    document.getElementById("key_and_con").innerHTML =
+        "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+        " manufactured in " + Erik.Vehicle_Year;
+}
+
+//nested function practice
+function counter() {
+    document.getElementById("Nested_Function").innerHTML = count();
+    function count() {
+        var start = 6;
+        function add_one() {
+            start += 1;
+        }
+        add_one();
+        return start;
+    }
 }
